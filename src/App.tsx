dcd5ed5100c1425a5db05358013981be1b79b9ff
Emmanuel <emmanuel.b.omont@gmail.com>
2024-07-10 22:24:40 +0200

    deploy final

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { act, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import deputesData from "./deputes.json";
 import './App.css';
 import { Tweet } from 'react-tweet'
